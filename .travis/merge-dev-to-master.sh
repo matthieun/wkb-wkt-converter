1894ee805cdd2765288b93a68e419efde44c83aa
matthieun <matthieu.nahoum@gmail.com>
2017-05-03 10:56:38 -0700

    Test out some script

--- .travis/merge-dev-to-master.sh
+++ .travis/merge-dev-to-master.sh
@@ -2,13 +2,18 @@
 
 GITHUB_REPO="matthieun/wkb-wkt-converter"
 MERGE_BRANCH=master
+SOIURCE_BRANCH=dev
 
 echo "merge-dev-to-master: $GITHUB_REPO"
+echo "merge-dev-to-master: TRAVIS_BRANCH = $TRAVIS_BRANCH"
+echo "merge-dev-to-master: TRAVIS_PULL_REQUEST = $TRAVIS_PULL_REQUEST"
 
-if [ "$TRAVIS_BRANCH" != "dev" || "$TRAVIS_PULL_REQUEST" != "false" ]; then
+if [[ "$TRAVIS_BRANCH" != "dev" || "$TRAVIS_PULL_REQUEST" != "false" ]]; then
 	echo "merge-dev-to-master: Exiting! Branch is not dev: $TRAVIS_BRANCH or this is a Pull Request: $TRAVIS_PULL_REQUEST"
     exit 0;
 fi
+
+exit 0;
 
 export GIT_COMMITTER_EMAIL="travis@travis.org"
 export GIT_COMMITTER_NAME="Travis CI"
