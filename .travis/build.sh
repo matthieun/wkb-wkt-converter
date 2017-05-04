fc6e90bb86e4b755a76b5fda37fffe19132d761f
matthieun <matthieun@users.noreply.github.com>
2017-05-04 13:20:16 -0700

    Add manual trigger shell script (#6)
    * Added travis build status
    
    * Move build script to .travis
    
    * Merge dev to master in travis script to allow failure
    
    * Add new line
    
    * Add then which was missing
    
    * Put after build in separate script
    
    * Added test logging
    
    * attempt at archive signing
    
    * remove whitespace
    
    * newline
    
    * newline
    
    * skip assemble too
    
    * fix scripts
    
    * fix scripts
    
    * Add manual trigger shell script
    
    * Fix indentation

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,6 +2,13 @@
 
 chmod u+x gradlew
 
+if [ "$MANUAL_RELEASE_TRIGGERED" = "true" ];
+then
+	# This is a release job, triggered manually
+	# Change the version locally to remove the -SNAPSHOT
+	sed -i "s/-SNAPSHOT//g" gradle.properties
+fi
+
 if [ "$TRAVIS_PULL_REQUEST" != "false" ];
 then
 	echo "Skip integration tests in pull request builds"
