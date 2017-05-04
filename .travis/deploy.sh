4e1cae06591266d3efdd6e06fa524c9172574aeb
matthieun <matthieun@users.noreply.github.com>
2017-05-03 18:09:58 -0700

    Archive signing and publishing (#5)
    * Added travis build status
    
    * Move build script to .travis
    
    * Merge dev to master in travis script to allow failure
    
    * Add new line
    
    * Add then which was missing
    
    * Put after build in separate script
    
    * Added test logging
    
    * attempt at archive signing
    
    * remove whitespace
    
    * newline
    
    * newline
    
    * skip assemble too
    
    * fix scripts
    
    * fix scripts

---
+++ .travis/deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ];
+then
+    ./gradlew signArchives
+    ./gradlew uploadArchives
+fi
--- .travis/merge-dev-to-master-gate.sh
+++ .travis/merge-dev-to-master-gate.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
 
-chmod u+x gradlew
-
 if [ $TRAVIS_TEST_RESULT -eq 0 ];
 then
 	.travis/merge-dev-to-master.sh
+	RETURN_VALUE=$?
+	if [ "$RETURN_VALUE" != "0" ];
+	then
+		exit $RETURN_VALUE
+	fi
 fi
